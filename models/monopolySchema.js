9568b7393d0c2fd72e29806a5d8ef5b76a20c7ea
eearly01 <elijah.early@gmail.com>
2023-02-24 11:13:25 -0600

    added the properties ejs, head ejs in partials, and worked on edit ejs

---
+++ models/monopolySchema.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const monopolySchema = new mongoose.Schema({
+	name: { String, required: true },
+	piece: { String, required: true },
+	money: { Number, default: 0 },
+	properties: [{
+        property: { 
+            propName: String,
+            value: Number
+        }
+    }],
+});
+
+const Monopoly = mongoose.model('Monopoly', monopolySchema);
+
+module.exports = Monopoly;
---
+++ models/propertiesData.js
@@ -0,0 +1,142 @@
+module.exports = [
+	{
+		propName: 'Mediterranean Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Baltic Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Oriental Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Vermont Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Connecticut Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'St. Charles Place',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'States Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Virginia Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'St. James Place',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Tennessee Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'New York Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Kentucky Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Indiana Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Illinois Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Atlantic Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Ventnor Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Marvin Gardens',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Pacific Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'North Carolina Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Pennsylvania Avenue',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Park Place',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Boardwalk',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Reading Railroad',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Pennsylvania Railroad',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'B. & O. Railroad',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Short Line',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Electric Company',
+		isPurchased: false,
+		value: 0,
+	},
+	{
+		propName: 'Water Works',
+		isPurchased: false,
+		value: 0,
+	},
+];
